cad555a0e7c0e08d3c53c8d78e472ee0bf191589
OlegKKK <kramo@ukr.net>
2016-11-23 10:05:26 +0200

    Task 2 Java If-Else

--- src/Introduction/SolutionC.java
+++ src/Introduction/SolutionC.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class SolutionC {
 
     public static void main(String[] args) {
-
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         String ans="";
