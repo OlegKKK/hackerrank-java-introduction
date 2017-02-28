e94700695ecae51cb72330a7f44d45598a157d0e
Oleg <kramo@ukr.net>
2017-02-28 21:52:35 +0200

    Task 8 Java End-of-file

---
+++ src/Introduction/SolutionI.java
@@ -0,0 +1,15 @@
+package Introduction;
+
+import java.util.*;
+
+public class SolutionI {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int i=1;
+        while(sc.hasNext()){
+            String s= sc.nextLine();
+            System.out.println(i+" "+s);
+            i++;
+        }
+    }
+}
